e72f72e117a3d67b91e0bfd4d33678cc58e4ee08
tmarjoram <tom.marjoram@gmail.com>
2017-08-29 07:36:42 +0100

    Sigmoid function in linear

---
+++ src/linear/nn.py
@@ -0,0 +1,71 @@
+'''
+Created on 28 Aug 2017
+
+@author: Dara
+'''
+"""
+NOTE: Here we're using an Input node for more than a scalar.
+In the case of weights and inputs the value of the Input node is
+actually a python list!
+
+In general, there's no restriction on the values that can be passed to an Input node.
+"""
+from miniflow import *
+import numpy as np
+
+inputs, weights, bias = Input(), Input(), Input()
+
+f = Linear(inputs, weights, bias)
+
+feed_dict = {
+    inputs: [6, 14, 3],
+    weights: [0.5, 0.25, 1.4],
+    bias: 2
+}
+
+graph = topological_sort(feed_dict)
+output = forward_pass(f, graph)
+
+print(output) # should be 12.7 with this example
+
+# Now for multiple nodes in a layer with multiple outputs
+X, W, b = Input(), Input(), Input()
+
+f = Linear(X, W, b)
+
+X_ = np.array([[-1., -2.], [-1, -2]])
+W_ = np.array([[2., -3], [2., -3]])
+b_ = np.array([-3., -5])
+
+feed_dict = {X: X_, W: W_, b: b_}
+
+graph = topological_sort(feed_dict)
+output = forward_pass(f, graph)
+"""
+Output should be:
+[[-9., 4.],
+[-9., 4.]]
+"""
+print(output)
+# sigmoid
+X, W, b = Input(), Input(), Input()
+
+f = Linear(X, W, b)
+g = Sigmoid(f)
+
+X_ = np.array([[-1., -2.], [-1, -2]])
+W_ = np.array([[2., -3], [2., -3]])
+b_ = np.array([-3., -5])
+
+feed_dict = {X: X_, W: W_, b: b_}
+
+graph = topological_sort(feed_dict)
+output = forward_pass(g, graph)
+
+"""
+Output should be:
+[[  1.23394576e-04   9.82013790e-01]
+ [  1.23394576e-04   9.82013790e-01]]
+"""
+print(output)
+
